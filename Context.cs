b46138fc785d34396c6a1d5c01fcaf5fef96f85d
unknown <jhonatanqsilva@gmail.com>
2018-08-21 16:39:08 -0300

    Arquivos iniciais

---
+++ Context.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioAmsterdam
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+          : base(options)
+        { }
+
+        public DbSet<TableHeaders> TableHeaders { get; set; }
+
+    }
+    public class TableHeaders
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TableID { get; set; }
+        public string TableName { get; set; }
+        public string HeaderValues { get; set; }
+    }
+}
---
+++ Migrations/20180821174518_TableHeaders.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesafioAmsterdam.Migrations
+{
+    public partial class TableHeaders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TableHeaders",
+                columns: table => new
+                {
+                    TableID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TableName = table.Column<string>(nullable: true),
+                    HeaderValues = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableHeaders", x => x.TableID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TableHeaders");
+        }
+    }
+}
